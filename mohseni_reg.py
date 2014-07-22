207293c9bdb7ec99d8a4e1d4ee7c7693109279b0
Matthew Bartos <matthew.d.bartos@gmail.com>
2014-07-21 20:15:50 -0700

    Update mohseni_reg.py

--- mohseni_reg.py
+++ mohseni_reg.py
@@ -9,12 +9,13 @@ from datetime import timedelta as td
 import ast
 from scipy.optimize import curve_fit
 from scipy.stats import gaussian_kde
+import matplotlib.pyplot as plt
 
 temp_d = pickle.load( open( "basin_temps.p", "rb"))
 r_latlon = pickle.load( open( "region_latlon.p", "rb"))
 latlon_d = pd.concat([i for i in r_latlon.values()], ignore_index=True)
 latlon_d = latlon_d.drop_duplicates()
-
+stat_cds = pd.read_csv('stat_cd_nm_query.asc', sep='\t', skiprows=7)
 
 class make_mohseni():
 	def __init__(self):
@@ -26,6 +27,7 @@ class make_mohseni():
 		self.param_d = {}
 		self.cat_d = {}
 		self.src_data = {}
+		self.isavg = {}
 		
 	def find_max_station(self):
 		for k in temp_d.keys():
@@ -85,38 +87,76 @@ class make_mohseni():
 	#		print diff_coords
 	#		print [i for i in t['datetime']]
 			w['date'] = [(date(int(i.split('-')[0]), int(i.split('-')[1]), int(i.split('-')[2]))) for i in w['datetime']]
-			tmax_str = [i for i in w.columns if '00010_00001' in i][0]
-			tmin_str = [i for i in w.columns if '00010_00002' in i][0]
-			tavg_str = [i for i in w.columns if '00010_00002' in i][0]
-			w['w_tmax'] = w[tmax_str]
-			w['w_tmin'] = w[tmin_str]
-			w = w[['date', 'site_no','w_tmax', 'w_tmin']]
+			col_li = [i for i in w.columns if '00010' in i]
+			temp_cols = [i for i in col_li if not 'cd' in i]
+			cd_cols = [i for i in col_li if 'cd' in i]
+			col_d = {'temp' : {}, 'cd' : {}}
+			for r in temp_cols:
+				if r.split('_')[2] == '00001':
+					col_d['temp'].update({'w_tmax' : r})
+				if r.split('_')[2] == '00002':
+					col_d['temp'].update({'w_tmin' : r})
+				if r.split('_')[2] == '00003':
+					col_d['temp'].update({'w_tavg' : r})
+				if r.split('_')[2] == '00008':
+					col_d['temp'].update({'w_tmed' : r})
+			for r in cd_cols:
+				if r.split('_')[2] == '00001':
+					col_d['cd'].update({'w_tmax_cd' : r})
+				if r.split('_')[2] == '00002':
+					col_d['cd'].update({'w_tmin_cd' : r})
+				if r.split('_')[2] == '00003':
+					col_d['cd'].update({'w_tavg_cd' : r})
+				if r.split('_')[2] == '00008':
+					col_d['cd'].update({'w_tmed_cd' : r})
+			cat_cols = ['date', 'site_no']
+			for e, v in col_d['temp'].items():
+				w[e] = w[v]
+				cat_cols.append(e)
+			for e, v in col_d['cd'].items():
+				w[e] = w[v]
+				cat_cols.append(e)
+			cat_cols = list(set(cat_cols))
+			w = w[[i for i in cat_cols]]
 			w = w.set_index('date')
 			w = w.replace(to_replace=['Ice'], value=[0.0])
-			w['w_tmax'] = [float(i) for i in w['w_tmax']]
-			w['w_tmin'] = [float(i) for i in w['w_tmin']]
-			w['w_tavg'] = 0.5*(w['w_tmin'] + w['w_tmax'])
-			a = pd.read_csv('./master/data_%s_%s' % (str(diff_coords[0]), str(diff_coords[1])), sep='\t', header=None, index_col=False, names=['year', 'month', 'day', 'prcp', 'tmax', 'tmin', 'wspd'])
-			a['tavg'] = 0.5*(a['tmin'] + a['tmax'])
-	#		d1 = date(1949, 1, 1)
-	#		d2 = date(2010, 12, 31)
-	#		ddelta = d2 - d1
-	#		dr = [d1 + td(days=i) for i in range(ddelta.days + 1)]
-	#		print len(dr)
-	#		print a[['year', 'month', 'day']]
-			a['date'] = [date(a['year'][i], a['month'][i], a['day'][i]) for i in a.index]
-	#		print a['date']
-			a = a.set_index('date')
-			c = pd.concat([w,a], axis=1)
-			print c
-			self.cat_d.update({basin : {}})
-			self.cat_d[basin].update({st_id : c})
+			for j in col_d['temp'].keys():
+				w[j] = w[j].convert_objects(convert_numeric=True)
+			if 'w_tavg' in w.columns:
+				self.isavg.update({st_id : True})
+			elif 'w_tmed' in w.columns:
+				w['w_tavg'] = w['w_tmed']
+				self.isavg.update({st_id : True})
+			elif ('w_tmax' in w.columns) and ('w_tmin' in w.columns):
+				w['w_tavg'] = 0.5*(w['w_tmin'] + w['w_tmax'])
+				self.isavg.update({st_id : True})
+			else:
+				self.isavg.update({st_id : False})
+			if self.isavg[st_id] == True:
+				a = pd.read_csv('./master/data_%s_%s' % (str(diff_coords[0]), str(diff_coords[1])), sep='\t', header=None, index_col=False, names=['year', 'month', 'day', 'prcp', 'tmax', 'tmin', 'wspd'])
+				a['tavg'] = 0.5*(a['tmin'] + a['tmax'])
+		#		d1 = date(1949, 1, 1)
+		#		d2 = date(2010, 12, 31)
+		#		ddelta = d2 - d1
+		#		dr = [d1 + td(days=i) for i in range(ddelta.days + 1)]
+		#		print len(dr)
+		#		print a[['year', 'month', 'day']]
+				a['date'] = [date(a['year'][i], a['month'][i], a['day'][i]) for i in a.index]
+		#		print a['date']
+				a = a.set_index('date')
+				c = pd.concat([w,a], axis=1)
+				print c
+				self.cat_d.update({basin : {}})
+				self.cat_d[basin].update({st_id : c})
+			else:
+				pass
 			
 		else:
 			print 'No stations'
 
 	def make_src_data(self, **src_kwargs):
-		self.src_data = self.cat_d[src_kwargs['dbasin']][src_kwargs['dst_id']].dropna()		
+		base_data = self.cat_d[src_kwargs['dbasin']][src_kwargs['dst_id']]
+		self.src_data = base_data.dropna()#.ix[(base_data['w_tmax_cd'] == 'A') & (base_data['w_tmin_cd'] == 'A')]
 		
 	def mohseni(self, x, b, g):
 		xdata = self.src_data['tavg']
@@ -141,44 +181,65 @@ class make_mohseni():
 			st_id = self.max_d[basin]
 		else:
 			st_id = kwargs['st_id']
-		self.make_src_data(dbasin=basin, dst_id=st_id)
-		xdata = self.src_data['tavg']
-		ydata = self.src_data['w_tavg']
-		b_init = 12.0
-		g_init = 0.2
-		popt, pcov = curve_fit(self.mohseni, xdata, ydata, p0=[b_init, g_init])
-		perr = np.sqrt(np.diag(pcov))
-		self.param_d.update({basin : {}})
-		self.param_d[basin].update({st_id : [popt, perr]})
-		print popt, perr
-
+		if self.isavg[st_id] == True:
+			self.make_src_data(dbasin=basin, dst_id=st_id)
+			xdata = self.src_data['tavg']
+			ydata = self.src_data['w_tavg']
+			if len(ydata) < 1:
+				pass
+			else:
+				b_init = 12.0
+				g_init = 0.2
+				popt, pcov = curve_fit(self.mohseni, xdata, ydata, p0=[b_init, g_init])
+				NSC = self.NS(self.mohseni(xdata, popt[0], popt[1]), ydata)
+				self.param_d.update({basin : {}})
+				self.param_d[basin].update({st_id : {}})
+				self.param_d[basin][st_id].update({'a': max(ydata), 'b': popt[0], 'g': popt[1], 'u': min(ydata), 'n': len(ydata), 'nsc': NSC})
+				print popt, NSC
+		else:
+			pass
 
 	def plot_curvefit(self, basin, **kwargs):
 		if kwargs['automax'] == True:
 			st_id = self.max_d[basin]
 		else:
 			st_id = kwargs['st_id']
-		fig, ax = plt.subplots(1)
-		xdata = self.src_data['tavg']
-		ydata = self.src_data['w_tavg']
-		NSC = self.NS(self.mohseni(xdata, self.param_d[basin][st_id][0][0], self.param_d[basin][st_id][0][1]), ydata)
-		print 'NSC', NSC
-		xy = np.vstack([xdata, ydata])
-		z = gaussian_kde(xy)(xy)
-		ax.scatter(xdata, ydata, c=z, s=100, edgecolor='')
-		ax.plot(arange(-20, 61), self.mohseni(arange(-20, 61), self.param_d[basin][st_id][0][0], self.param_d[basin][st_id][0][1]), c='black')
-		statname = self.loc_d[basin]['STATION_NM'].ix[m.loc_d[basin]['SITE_NO'] == st_id]
-		statname = statname.values[0]
-		print statname
-		title('USGS STATION %s, %s' % (self.max_d[basin], statname))
-		xlabel('Air Temperature ($^\circ$C)')
-		ylabel('Stream Temperature ($^\circ$C)')
-		ylim([0, max(self.src_data['w_tavg']) + 5])
-		xlim([-20, 60])
-#		textstr = r'$\beta=%.2f$\n$\gamma=%.2f$\n' % (self.param_d[basin][0][0], self.param_d[basin][0][1])
-		textstr = '$\\alpha=%.2f$\n$\\beta=%.2f$\n$\\gamma=%.2f$\n$\\mu=%.2f$\n$n=%s$\n$N.S.C.=%.2f$' % (max(self.src_data['w_tavg']), self.param_d[basin][st_id][0][0], self.param_d[basin][st_id][0][1], min(self.src_data['w_tavg']), str(len(ydata)), NSC)
-		props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-		ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', linespacing=1.25, bbox=props)
+		if self.isavg[st_id] == True:
+			fig, ax = plt.subplots(1)
+			xdata = self.src_data['tavg']
+			ydata = self.src_data['w_tavg']
+			if len(ydata) < 1:
+				pass
+			else:
+				NSC = self.param_d[basin][st_id]['nsc']
+				print 'NSC', NSC
+				xy = np.vstack([xdata, ydata])
+				z = gaussian_kde(xy)(xy)
+				ax.scatter(xdata, ydata, c=z, s=100, edgecolor='')
+				ax.plot(np.arange(-20, 61), self.mohseni(np.arange(-20, 61), self.param_d[basin][st_id]['b'], self.param_d[basin][st_id]['g']), c='black')
+				statname = self.loc_d[basin]['STATION_NM'].ix[m.loc_d[basin]['SITE_NO'] == st_id]
+				statname = statname.values[0]
+				print statname
+				plt.title('USGS STATION %s, %s' % (self.max_d[basin], statname))
+				plt.xlabel('Air Temperature ($^\circ$C)')
+				plt.ylabel('Stream Temperature ($^\circ$C)')
+				plt.ylim([0, max(self.src_data['w_tavg']) + 5])
+				plt.xlim([-20, 60])
+#				textstr = r'$\beta=%.2f$\n$\gamma=%.2f$\n' % (self.param_d[basin][0][0], self.param_d[basin][0][1])
+				textstr = '$\\alpha=%.2f$\n$\\beta=%.2f$\n$\\gamma=%.2f$\n$\\mu=%.2f$\n$n=%s$\n$N.S.C.=%.2f$' % (self.param_d[basin][st_id]['a'], self.param_d[basin][st_id]['b'], self.param_d[basin][st_id]['g'], self.param_d[basin][st_id]['u'], self.param_d[basin][st_id]['n'], self.param_d[basin][st_id]['nsc'])
+				props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+				ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=14, verticalalignment='top', linespacing=1.25, bbox=props)
+				if 'img' in os.listdir('.'):
+					pass
+				else:
+					os.mkdir('./img')
+				if basin in os.listdir('./img'):
+					pass
+				else:
+					os.mkdir('./img/%s' % (basin))
+				plt.savefig('./img/%s/%s.png' % (basin, st_id), bbox_inches='tight')
+		else:
+			pass
 		
 	def prep_data(self, basin, **kwargs):
 		self.find_max_station()
@@ -193,12 +254,21 @@ class make_mohseni():
 		
 	def reg_exec(self, basin, **kwargs):
 		if kwargs['automax'] == True:
-			self.fit_mohseni(basin, automax=True)
-			self.plot_curvefit(basin, automax=True)
+			try:
+				self.fit_mohseni(basin, automax=True)
+			except RuntimeError:
+				self.param_d[basin][st_id].update({'a': 'NA', 'b': 'NA', 'g': 'NA', 'u': 'NA', 'n': 'NA', 'nsc': 'NA'})
+			else:
+				self.plot_curvefit(basin, automax=True)
 		else:
 			st_id = kwargs['st_id']
-			self.fit_mohseni(basin, automax=False, st_id=st_id)
-			self.plot_curvefit(basin, automax=False, st_id=st_id)			
+			try:
+				self.fit_mohseni(basin, automax=False, st_id=st_id)
+			except RuntimeError:
+				self.param_d[basin].update({st_id : {}})
+				self.param_d[basin][st_id].update({'a': 'NA', 'b': 'NA', 'g': 'NA', 'u': 'NA', 'n': 'NA', 'nsc': 'NA'})
+			else:
+				self.plot_curvefit(basin, automax=False, st_id=st_id)
 		
 
 m = make_mohseni()
@@ -206,11 +276,94 @@ m.prep_data('lees_f', automax=True)
 m.reg_exec('lees_f', automax=True)
 m.prep_data('pitt', automax=True)
 m.reg_exec('pitt', automax=True)
+m.prep_data('pawnee', automax=True)
+m.reg_exec('pawnee', automax=True)
+m.prep_data('comanche', automax=True)
+m.reg_exec('comanche', automax=True)
+m.prep_data('glenn', automax=True)
+m.reg_exec('glenn', automax=True)
+m.prep_data('paper', automax=True)
+m.reg_exec('paper', automax=True)
+m.prep_data('corona', automax=True)
+m.reg_exec('corona', automax=True)
+
+
 
 m = make_mohseni()
 m.prep_data('lees_f', automax=False, st_id='09095500')
 m.reg_exec('lees_f', automax=False, st_id='09095500')
 
+
+m = make_mohseni()
+for q in temp_d['comanche'].keys():
+	m.prep_data('comanche', automax=False, st_id=q)
+	m.reg_exec('comanche', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['paper'].keys():
+	m.prep_data('paper', automax=False, st_id=q)
+	m.reg_exec('paper', automax=False, st_id=q)
+	
+m = make_mohseni()
+for q in temp_d['glenn'].keys():
+	m.prep_data('glenn', automax=False, st_id=q)
+	m.reg_exec('glenn', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['pawnee'].keys():
+	m.prep_data('pawnee', automax=False, st_id=q)
+	m.reg_exec('pawnee', automax=False, st_id=q)
+	
+m = make_mohseni()
+for q in temp_d['pitt'].keys():
+	m.prep_data('pitt', automax=False, st_id=q)
+	m.reg_exec('pitt', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['colstrip'].keys():
+	m.prep_data('colstrip', automax=False, st_id=q)
+	m.reg_exec('colstrip', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['intermtn'].keys():
+	m.prep_data('intermtn', automax=False, st_id=q)
+	m.reg_exec('intermtn', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['lees_f'].keys():
+	m.prep_data('lees_f', automax=False, st_id=q)
+	m.reg_exec('lees_f', automax=False, st_id=q)
+	
+m = make_mohseni()
+for q in temp_d['brigham'].keys():
+	m.prep_data('brigham', automax=False, st_id=q)
+	m.reg_exec('brigham', automax=False, st_id=q)
+	
+m = make_mohseni()
+for q in temp_d['brigham'].keys():
+	m.prep_data('brigham', automax=False, st_id=q)
+	m.reg_exec('brigham', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['corona'].keys():
+	m.prep_data('corona', automax=False, st_id=q)
+	m.reg_exec('corona', automax=False, st_id=q)
+	
+m = make_mohseni()
+for q in temp_d['lahontan'].keys():
+	m.prep_data('lahontan', automax=False, st_id=q)
+	m.reg_exec('lahontan', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['guer'].keys():
+	m.prep_data('guer', automax=False, st_id=q)
+	m.reg_exec('guer', automax=False, st_id=q)
+
+m = make_mohseni()
+for q in temp_d['salton'].keys():
+	m.prep_data('salton', automax=False, st_id=q)
+	m.reg_exec('salton', automax=False, st_id=q)
+	
 m = make_mohseni()
 m.find_max_station()
 m.make_loc_d()
