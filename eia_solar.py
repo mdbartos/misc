561871d00fb0e4341e106bf6902905491f75c227
Matthew Bartos <matthew.d.bartos@gmail.com>
2014-11-26 01:39:36 -0700

    new file:   .eia_solar.py.un~ 	modified:   eia_solar.py

--- eia_solar.py
+++ eia_solar.py
@@ -37,4 +37,45 @@ for i in solar.index:
     solar.loc[i, 'lat_grid'] = lat_grid
     solar.loc[i, 'lon_grid'] = lon_grid
 
+##########SUNY GRID FOR SOLAR RADIATION
 
+lats_suny = pd.Series(np.arange(southbound, northbound, 0.05)[1:][::2])
+longs_suny = pd.Series(np.arange(westbound, eastbound, 0.05)[1:][::2])
+
+solar['lat_suny'] = 0.0
+solar['lon_suny'] = 0.0
+
+for i in solar.index:
+    sitelat = solar.loc[i, 'Latitude']
+    sitelon = solar.loc[i, 'Longitude']
+    lat_grid = lats_suny.loc[abs((lats_suny - sitelat)).idxmin()]
+    lon_grid = longs_suny.loc[abs((longs_suny - sitelon)).idxmin()]
+    solar.loc[i, 'lat_suny'] = lat_grid
+    solar.loc[i, 'lon_suny'] = lon_grid
+
+
+#######################################
+import math
+import subprocess as sub
+
+basestr = 'ftp://ftp.ncdc.noaa.gov/pub/data/nsrdb-solar/SUNY-gridded-data'
+
+for g in list(set(zip(solar['lat_suny'], solar['lon_suny']))):  
+    lat = g[0]
+    lon = g[1]
+    grid_lats = range(24,51)[::2]
+    grid_lons = range(66,129)[::2]
+    closest_lat = max([i for i in grid_lats if i < lat])
+    closest_lon = min([i for i in grid_lons if i > abs(lon)])
+    dirstr = '%s%s' % (closest_lon, closest_lat)
+    
+    latstr = str(abs(lat)).replace('.', '')
+    lonstr = str(abs(lon)).replace('.', '')
+
+    filestr = 'SUNY_%s%s.csv.gz' % (lonstr, latstr)
+
+    cmd = "wget --random-wait -P /home/tabris/Downloads/suny_solar/ %s/%s/%s" % (basestr, dirstr, filestr)
+    sub.call(cmd, shell=True)
+
+#    lats = list(set([int(i) for i in solar['lat_suny']]))
+#    lons = list(set([int(i) for i in solar['lon_suny']]))
